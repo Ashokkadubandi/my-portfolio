099b5138ff68614c7a44994755752177bd00da4a
AshokKadubandi <velmaashok765@gmail.com>
2025-02-23 11:09:49 +0530

    update carousel

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 let projectBoxes = document.querySelectorAll('.project-card');
 let proImg = document.querySelector('.ban-curve-card')
-
 let boxHeight = window.innerHeight
+let Cardcarousel = document.querySelector('.card');
+let boxes = document.querySelectorAll(".scroller");
+let dotBoxes = document.querySelectorAll(".dot");
 
 const getScroller = () => {
-    projectBoxes.forEach((box) => {
-        let clientHeight = box.getBoundingClientRect().top
-        if(clientHeight < innerHeight / 5 * 4){
-            box.classList.add('scroll');
-        }else{
-            box.classList.remove('scroll')
-        }
-    })
+        boxes.forEach(eachBox => {
+            let boxClient = eachBox.getBoundingClientRect().left
+            if(boxClient < 50 && boxClient > 0){
+                dotBoxes.forEach(box => {
+                    if(box.id === eachBox.id){
+                        box.classList.add("opacity")
+                    }else{
+                        box.classList.remove("opacity");
+                    }
+                })
+            }
+        })
+    }
 
-}
+Cardcarousel.addEventListener("scroll",function(e){
+    getScroller()
+})
 
-window.addEventListener("scroll",getScroller)
+getScroller()
